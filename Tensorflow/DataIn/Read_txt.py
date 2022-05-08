233e60a6348b9940a93128690a2b4921674169ed
Ling Huang <linghypshen@gmail.com>
2022-05-08 15:36:18 -0400

    add code

--- Tensorflow/DataIn/Read_txt.py
+++ Tensorflow/DataIn/Read_txt.py
@@ -16,5 +16,17 @@ if __name__ == "__main__":
     for example in frank_dataset.take(5):
         print(example)
 
-    
+    # imdb_url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    # tf.keras.utils.get_file("aclImdb_v1.tar.gz", imdb_url, extract=True, cache_dir=cache_dir, cache_subdir=cache_subdir)
 
+    imdb_dir = f"{cache_dir}/{cache_subdir}/aclImdb"
+    imdb_train_dataset = tf.keras.preprocessing.text_dataset_from_directory(
+        f"{imdb_dir}/train",
+        label_mode="binary",
+        batch_size=32,
+        seed=42
+    )
+
+    for example, label in imdb_train_dataset.take(1):
+        print(example.numpy()[0])
+        print(label[0])
