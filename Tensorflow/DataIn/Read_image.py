54b6e48f004fa741039184ad2d0a74a8159701da
Ling Huang <linghypshen@gmail.com>
2022-05-08 10:09:31 -0400

    add code

--- Tensorflow/DataIn/Read_image.py
+++ Tensorflow/DataIn/Read_image.py
@@ -15,3 +15,16 @@ if __name__ == "__main__":
     class_zero_images = os.listdir(f"{extract_path}/0") ## directory with images
     im = Image.open(f"{extract_path}/0/{class_zero_images[0]}")
     plt.imshow(im, cmap="Greys")
+    plt.show()
+
+    test_preprocess = keras.preprocessing.images.ImageDataGenerator(rescale=1.0/255)
+
+    test_generator = test_preprocess.flow_from_directory(
+        extract_path,
+        target_size=(28, 28),
+        color_mode='grayscale',
+        class_mode='categorical',
+        batch_size=2,
+        shuffle=True,
+
+    )
