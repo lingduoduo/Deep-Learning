78418de47b8645d88c59982da5b59f417ef02305
Ling Huang <linghypshen@gmail.com>
2022-05-08 15:13:09 -0400

    add code

--- Tensorflow/DataIn/Read_image.py
+++ Tensorflow/DataIn/Read_image.py
@@ -11,14 +11,15 @@ if __name__ == "__main__":
     cache_subdir = "data"
     keras.utils.get_file("fmnist_test.tgz", url, extract=True, cache_dir=cache_dir, cache_subdir=cache_subdir)
 
+    # plot one image
     extract_path = f"{cache_dir}/{cache_subdir}/fmnist_test"
-    class_zero_images = os.listdir(f"{extract_path}/0") ## directory with images
+    class_zero_images = os.listdir(f"{extract_path}/0") ## list of images under the directory
     im = Image.open(f"{extract_path}/0/{class_zero_images[0]}")
     plt.imshow(im, cmap="Greys")
     plt.show()
 
-    test_preprocess = keras.preprocessing.images.ImageDataGenerator(rescale=1.0/255)
-
+    # load all images for training
+    test_preprocess = keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
     test_generator = test_preprocess.flow_from_directory(
         extract_path,
         target_size=(28, 28),
@@ -26,5 +27,8 @@ if __name__ == "__main__":
         class_mode='categorical',
         batch_size=2,
         shuffle=True,
-
+        seed=42
     )
+
+    test_batch = test_generator.next()
+
