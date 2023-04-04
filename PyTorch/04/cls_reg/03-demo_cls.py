f3220fe0c9154766671fe072142b8c1c71002902
Ling Huang <linghypshen@gmail.com>
2023-04-03 23:54:26 -0400

    add code

--- PyTorch/04/cls_reg/03-demo_cls.py
+++ PyTorch/04/cls_reg/03-demo_cls.py
@@ -3,40 +3,42 @@ import torchvision.datasets as dataset
 import torchvision.transforms as transforms
 import torch.utils.data as data_utils
 from CNN import CNN
-#data
+
+# data
 train_data = dataset.MNIST(root="mnist",
                            train=True,
                            transform=transforms.ToTensor(),
                            download=True)
 
 test_data = dataset.MNIST(root="mnist",
-                           train=False,
-                           transform=transforms.ToTensor(),
-                           download=False)
-#batchsize
+                          train=False,
+                          transform=transforms.ToTensor(),
+                          download=False)
+# batchsize
 train_loader = data_utils.DataLoader(dataset=train_data,
                                      batch_size=64,
                                      shuffle=True)
 
 test_loader = data_utils.DataLoader(dataset=test_data,
-                                     batch_size=64,
-                                     shuffle=True)
+                                    batch_size=64,
+                                    shuffle=True)
 
 cnn = CNN()
-cnn = cnn.cuda()
-#loss
+# cnn = cnn.cuda()
+
+# loss
 
 loss_func = torch.nn.CrossEntropyLoss()
 
-#optimizer
+# optimizer
 
 optimizer = torch.optim.Adam(cnn.parameters(), lr=0.01)
 
-#training
+# # training
 for epoch in range(10):
     for i, (images, labels) in enumerate(train_loader):
-        images = images.cuda()
-        labels = labels.cuda()
+        # images = images.cuda()
+        # labels = labels.cuda()
 
         outputs = cnn(images)
         loss = loss_func(outputs, labels)
@@ -46,18 +48,18 @@ for epoch in range(10):
         optimizer.step()
 
     print("epoch is {}, ite is "
-          "{}/{}, loss is {}".format(epoch+1, i,
+          "{}/{}, loss is {}".format(epoch + 1, i,
                                      len(train_data) // 64,
                                      loss.item()))
-    #eval/test
+    # eval/test
     loss_test = 0
     accuracy = 0
     for i, (images, labels) in enumerate(test_loader):
-        images = images.cuda()
-        labels = labels.cuda()
+        # images = images.cuda()
+        # labels = labels.cuda()
         outputs = cnn(images)
-        #[batchsize]
-        #outputs = batchsize * cls_num
+        # [batchsize]
+        # outputs = batchsize * cls_num
         loss_test += loss_func(outputs, labels)
         _, pred = outputs.max(1)
         accuracy += (pred == labels).sum().item()
@@ -71,8 +73,3 @@ for epoch in range(10):
                                    loss_test.item()))
 
 torch.save(cnn, "model/mnist_model.pkl")
-
-
-
-
-
