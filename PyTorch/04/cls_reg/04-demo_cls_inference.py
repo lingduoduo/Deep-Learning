e7203268bbeff1b002b6d3c018a330d136f962ac
Ling Huang <linghypshen@gmail.com>
2023-04-03 23:54:26 -0400

    add code

--- PyTorch/04/cls_reg/04-demo_cls_inference.py
+++ PyTorch/04/cls_reg/04-demo_cls_inference.py
@@ -15,7 +15,8 @@ test_loader = data_utils.DataLoader(dataset=test_data,
                                     shuffle=True)
 
 cnn = torch.load("model/mnist_model.pkl")
-cnn = cnn.cuda()
+# cnn = cnn.cuda()
+
 #loss
 #eval/test
 loss_test = 0
@@ -23,18 +24,16 @@ accuracy = 0
 
 import cv2
 
-
-#pip install opencv-python -i http://mirrors.aliyun.com/pypi/simple/   --trusted-host mirrors.aliyun.com
 for i, (images, labels) in enumerate(test_loader):
-    images = images.cuda()
-    labels = labels.cuda()
+    # images = images.cuda()
+    # labels = labels.cuda()
     outputs = cnn(images)
     _, pred = outputs.max(1)
     accuracy += (pred == labels).sum().item()
 
-    images = images.cpu().numpy()
-    labels = labels.cpu().numpy()
-    pred = pred.cpu().numpy()
+    # images = images.cpu().numpy()
+    # labels = labels.cpu().numpy()
+    # pred = pred.cpu().numpy()
     #batchsize * 1 * 28 * 28
 
     for idx in range(images.shape[0]):
@@ -42,13 +41,8 @@ for i, (images, labels) in enumerate(test_loader):
         im_label = labels[idx]
         im_pred = pred[idx]
         im_data = im_data.transpose(1, 2, 0)
+
 accuracy = accuracy / len(test_data)
 print(accuracy)
-
-
-
-
-
-
 
 
