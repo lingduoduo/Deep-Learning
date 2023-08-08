b605fb1273012ac24288b5ebb269278be1c15163
Ling Huang <linghypshen@gmail.com>
2023-08-07 23:45:18 -0400

    add code

--- Tensorflow/Dive-into-Deep-Learning/6_3.py
+++ Tensorflow/Dive-into-Deep-Learning/6_3.py
@@ -5,10 +5,11 @@ import tensorflow as tf
 # 此函数初始化卷积层权重，并对输入和输出提高和缩减相应的维数
 def comp_conv2d(conv2d, X):
     # 这里的（1，1）表示批量大小和通道数都是1
-    X = tf.reshape(X, (1, ) + X.shape + (1, ))
+    X = tf.reshape(X, (1,) + X.shape + (1,))
     Y = conv2d(X)
     # 省略前两个维度：批量大小和通道
     return tf.reshape(Y, Y.shape[1:3])
+
 
 # 请注意，这里每边都填充了1行或1列，因此总共添加了2行或2列
 conv2d = tf.keras.layers.Conv2D(1, kernel_size=3, padding='same')
@@ -21,5 +22,5 @@ print(comp_conv2d(conv2d, X).shape)
 conv2d = tf.keras.layers.Conv2D(1, kernel_size=3, padding='same', strides=2)
 print(comp_conv2d(conv2d, X).shape)
 
-conv2d = tf.keras.layers.Conv2D(1, kernel_size=(3,5), padding='valid', strides=(3, 4))
+conv2d = tf.keras.layers.Conv2D(1, kernel_size=(3, 5), padding='valid', strides=(3, 4))
 print(comp_conv2d(conv2d, X).shape)
