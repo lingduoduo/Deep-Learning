14ed245766ce7f23a37511da80f95299c5fc9e2b
Ling Huang <linghypshen@gmail.com>
2023-08-14 19:09:14 -0400

    add code

--- Dive-into-Deep-Learning/code/Dive-into-Deep-Learning/06_6_LeNet.py
+++ Dive-into-Deep-Learning/code/Dive-into-Deep-Learning/06_6_LeNet.py
@@ -58,10 +58,39 @@ def try_gpu(i=0):
         return tf.device(f'/GPU:{i}')
     return tf.device('/CPU:0')
 
+
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        """Defined in :numref:`subsec_linear_model`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
 class TrainCallback(tf.keras.callbacks.Callback):  #@save
     """一个以可视化的训练进展的回调"""
     def __init__(self, net, train_iter, test_iter, num_epochs, device_name):
-        self.timer = d2l.Timer()
+        self.timer = Timer()
         self.animator = d2l.Animator(
             xlabel='epoch', xlim=[1, num_epochs], legend=[
                 'train loss', 'train acc', 'test acc'])
@@ -89,9 +118,10 @@ class TrainCallback(tf.keras.callbacks.Callback):  #@save
             print(f'{num_examples / self.timer.avg():.1f} examples/sec on '
                   f'{str(self.device_name)}')
 
-#@save
 def train_ch6(net_fn, train_iter, test_iter, num_epochs, lr, device):
-    """用GPU训练模型(在第六章定义)"""
+    """Train a model with a GPU (defined in Chapter 6).
+
+    Defined in :numref:`sec_lenet`"""
     device_name = device._device_name
     strategy = tf.distribute.OneDeviceStrategy(device_name)
     with strategy.scope():
@@ -99,7 +129,8 @@ def train_ch6(net_fn, train_iter, test_iter, num_epochs, lr, device):
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         net = net_fn()
         net.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
-    callback = TrainCallback(net, train_iter, test_iter, num_epochs, device_name)
+    callback = TrainCallback(net, train_iter, test_iter, num_epochs,
+                             device_name)
     net.fit(train_iter, epochs=num_epochs, verbose=0, callbacks=[callback])
     return net
 
