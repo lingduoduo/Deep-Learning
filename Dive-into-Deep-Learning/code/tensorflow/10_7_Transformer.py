aa0c77316c33723e0eabf157c41a37a710668671
Ling Huang <linghypshen@gmail.com>
2023-08-16 23:16:56 -0400

    add code

--- Dive-into-Deep-Learning/code/tensorflow/10_7_Transformer.py
+++ Dive-into-Deep-Learning/code/tensorflow/10_7_Transformer.py
@@ -116,26 +116,6 @@ class DotProductAttention(tf.keras.layers.Layer):
 
     # Shape of `queries`: (`batch_size`, no. of queries, `d`)
     # Shape of `keys`: (`batch_size`, no. of key-value pairs, `d`)
-    # Shape of `values`: (`batch_size`, no. of key-value pairs, value dimension)
-    # Shape of `valid_lens`: (`batch_size`,) or (`batch_size`, no. of queries)
-    def call(self, queries, keys, values, valid_lens, **kwargs):
-        d = queries.shape[-1]
-        scores = tf.matmul(queries, keys, transpose_b=True)/tf.math.sqrt(
-            tf.cast(d, dtype=tf.float32))
-        self.attention_weights = masked_softmax(scores, valid_lens)
-        return tf.matmul(self.dropout(self.attention_weights, **kwargs), values)
-
-
-class DotProductAttention(tf.keras.layers.Layer):
-    """Scaled dot product attention.
-
-    Defined in :numref:`subsec_additive-attention`"""
-    def __init__(self, dropout, **kwargs):
-        super().__init__(**kwargs)
-        self.dropout = tf.keras.layers.Dropout(dropout)
-
-    # Shape of `queries`: (`batch_size`, no. of queries, `d`)
-    # Shape of `keys`: (`batch_size`, no. of key-value pairs, `d`)
     # Shape of `values`: (`batch_size`, no. of key-value pairs, value
     # dimension)
     # Shape of `valid_lens`: (`batch_size`,) or (`batch_size`, no. of queries)
