1aa633d93933698ce388dbe33f527684886561d8
Ling <linghypshen@gmail.com>
2024-05-14 11:58:18 -0400

    add code

--- PyTorch/03/02-demo_attribute.py
+++ PyTorch/03/02-demo_attribute.py
@@ -13,3 +13,29 @@ a = torch.sparse_coo_tensor(i, v, (4, 4),
                             dtype=torch.float32,
                             device=dev).to_dense()
 print(a)
+
+
+tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+
+# index single element
+element = tensor[0, 0]  # 1
+
+# slice a row
+slice_1 = tensor[0, :]  # [1, 2, 3]
+slice_2 = tensor[:, 1]  # [2, 5]
+
+# change element
+tensor[0, 0] = 7
+print(tensor)  # [[7, 2, 3], [4, 5, 6]]
+
+# shape attribute
+shape = tensor.shape  # torch.Size([2, 3])
+print(shape)
+
+# unsqueeze(dim) increase the dimension of the tensor by one
+expanded_tensor = tensor.unsqueeze(0)  #  new shape = [1, 2, 3]
+print(expanded_tensor)
+
+# reshape(shape) reshpe the tensor to the given shape
+reshaped_tensor = tensor.reshape(6)  # new shape = [6]
+print(reshaped_tensor)
--- PyTorch/03/06-demo_compare.py
+++ PyTorch/03/06-demo_compare.py
@@ -16,6 +16,9 @@ print(torch.le(a, b))
 print(torch.lt(a, b))
 print(torch.ne(a, b))
 
+# return a tensor with all elements set to False
+greater_than = tensor > tensor
+
 ####
 
 a = torch.tensor([[1, 4, 4, 3, 5],
