57410d389dacaa3ff04d60b5c995b4082c33ac45
linghypshen@gmail.com <linghypshen@gmail.com>
2026-08-24 16:07:24 -0400

    add code

--- PyTorch-Models/00_Functions.py
+++ PyTorch-Models/00_Functions.py
@@ -138,3 +138,39 @@ loss = accumulated_step(model, opt, nn.MSELoss(),
 print('Loss:', loss)
 
 
+class MyAdam:
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8):
+        self.params = list(params)
+        self.lr = lr
+        self.beta1, self.beta2 = betas
+        self.eps = eps
+        self.t = 0
+        self.m = [torch.zeros_like(p) for p in self.params]
+        self.v = [torch.zeros_like(p) for p in self.params]
+
+    def step(self):
+        self.t += 1
+        with torch.no_grad():
+            for i, p in enumerate(self.params):
+                if p.grad is None:
+                    continue
+                self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * p.grad
+                self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * p.grad ** 2
+                m_hat = self.m[i] / (1 - self.beta1 ** self.t)
+                v_hat = self.v[i] / (1 - self.beta2 ** self.t)
+                p -= self.lr * m_hat / (torch.sqrt(v_hat) + self.eps)
+
+    def zero_grad(self):
+        for p in self.params:
+            if p.grad is not None:
+                p.grad.zero_()
+torch.manual_seed(0)
+w = torch.randn(4, 3, requires_grad=True)
+opt = MyAdam([w], lr=0.01)
+for i in range(5):
+    loss = (w ** 2).sum()
+    loss.backward()
+    opt.step()
+    opt.zero_grad()
+    print(f'Step {i}: loss={loss.item():.4f}')
+        
